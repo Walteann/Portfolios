00547e40fc79803da6d7605f943e887867c1b9b4
Walteann <walteann3@gmail.com>
2017-10-07 21:07:24 -0300

    Pagina Helio Completa com Email

---
+++ HelioPortfolio/enviaMsg.php
@@ -0,0 +1,36 @@
+﻿<?php 
+
+
+$nome = $_POST['nome'];
+$email = $_POST['email'];
+$assunto = $_POST['assunto'];
+$msg = $_POST['mensagem'];
+
+
+$cabecalho = "From: ". $nome;
+
+
+$corpoEmail =  "Fale Conosco - Hélio barros
+                
+                Nome:       $nome
+                Email:      $email
+                Assunto:    $assunto
+                
+                Mensagem:   $msg";
+
+
+if(mail('heliomiguelbarros@gmail.com', "Fale Conosco - Hélio Barros", $corpoEmail, $cabecalho)){
+    
+    
+    echo "<script>alert('Mensagem Enviada com Sucesso!!');</script>";
+    echo "<script>window.location.assign('https://heliobarros.000webhostapp.com');</script>";
+}else{
+    echo "<script>alert('Erro ao enviarm tente diretamente pelo email walteann3@gmail.com ');</script>";
+}
+
+
+
+
+?>
+
+
--- HelioPortfolio/index.php
+++ HelioPortfolio/index.php
@@ -1,14 +1,22 @@
-<!DOCTYPE html>
+﻿<!DOCTYPE html>
 <html>
-<title>Hélio Barros</title>
+<head>
+    <title>Hélio Barros</title>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="css/bootstrap-css/bootstrap.min.css">
+
 <link rel="stylesheet" href="css/style.css">
+
 <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato">
 <link rel="stylesheet" href="css/font-awesome4/css/font-awesome.min.css">
 <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+<link rel="stylesheet" href="css/meuEstiloCSS.css">
+
+
+<script src="js/jquery3.2.1.min.js"></script>
 <style>
-    body,
+  body,
     h1,
     h2,
     h3,
@@ -37,13 +45,13 @@
     /* First image (Logo. Full height) */
     
     .bgimg-1 {
-        background-image: url('/imagens/engrenagem.jpg');
+        background-image: url('imagens/engrenagem.jpg');
         min-height: 100%;
     }
     /* Second image (Portfolio) */
     
     .bgimg-2 {
-        background-image: url("/imagens/helio_bkg3.jpg");
+        background-image: url("imagens/helio_bkg3.jpg");
         min-height: 400px;
     }
     /* Third image (Contact) */
@@ -60,7 +68,15 @@
     .walt-hover-opacity {
         cursor: pointer;
     }
-    /* Turn off parallax scrolling for tablets and phones */
+    
+    
+
+    
+    fieldset{
+display: flex;
+text-align: center;
+}
+    /* Turn off parallax scrolling for tablets and phones Testa DPS*/
     
     @media only screen and (max-device-width: 1024px) {
         .bgimg-1,
@@ -71,44 +87,79 @@
     }
 </style>
 
+
+<script>
+    
+   // $(function() {
+            var nav = $('#minhaNavegacao');
+            $(window).scroll(function() {
+                if ($(this).scrollTop() > 150) {
+                    nav.addClass("navbar-inverse");
+                    nav.removeClass("navbar-default");
+                } else {
+                    nav.removeClass("navbar-inverse");
+                    nav.addClass("navbar-default");
+                }
+            });
+       // });
+    
+
+    jQuery(document).ready(function($){
+        
+        
+        /* Efeito scroll suave */
+        $('.scrollSuave').click(function(event){
+           event.preventDefault();
+            $('html,body').animate({scrollTop:$(this.hash).offset().top}, 800);
+        });
+    });
+    
+</script>
+
+</head>
+
+
 <body>
-
-    <!-- Navbar (sit on top) -->
-    <div class="walt-top">
-        <div class="walt-bar" id="myNavbar">
-            <a class="walt-bar-item walt-button walt-hover-black walt-hide-medium walt-hide-large walt-right" href="javascript:void(0);" onclick="toggleFunction()" title="Toggle Navigation Menu">
-                <i class="fa fa-bars"></i>
-            </a>
-            <a href="#home" class="walt-bar-item walt-button">HÉLIO BARROS</a>
-            <a href="#about" class="walt-bar-item walt-button walt-hide-small"><i class="fa fa-user"></i> SOBRE MIM</a>
-            <a href="#portfolio" class="walt-bar-item walt-button walt-hide-small"><i class="fa fa-th"></i> FORMAÇÃO</a>
-            <a href="#contact" class="walt-bar-item walt-button walt-hide-small"><i class="fa fa-envelope"></i> CONTATO</a>
-
+    
+    <nav id="minhaNavegacao" class="navbar navbar-inverse navbar-fixed-top" role="navigation" id="navegacao">
+      <div class="container-fluid">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#minhaNav">
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>                        
+          </button>
+          <a id="logo-marca" class="navbar-brand" href="#">Hélio Barros </a><img style="margin-left: -10px;" src="imagens/Engranagem_icones.png" alt="">
         </div>
+        <div class="collapse navbar-collapse" id="minhaNav">
 
-        <!-- Navbar on small screens -->
-        <div id="navDemo" class="walt-bar-block walt-white walt-hide walt-hide-large walt-hide-medium">
-            <a href="#about" class="walt-bar-item walt-button" onclick="toggleFunction()">SOBRE MIM</a>
-            <a href="#portfolio" class="walt-bar-item walt-button" onclick="toggleFunction()">FORMAÇÃO</a>
-            <a href="#contact" class="walt-bar-item walt-button" onclick="toggleFunction()">CONTATO</a>
+          <ul class="nav navbar-nav navbar-right">
+            <li><a class="scrollSuave" href="#sobreHelio">SOBRE MIM</a></li>
+            <li><a class="scrollSuave" href="#formacaoProf">FORMAÇÃO</a></li>
+            <li><a class="scrollSuave" href="#contatoComHelio">CONTATO</a></li>
+            <li></li>
+          </ul>
         </div>
-    </div>
+      </div>
+    </nav>
 
     <!-- First Parallax Image with Logo Text -->
     <div class="bgimg-1 walt-display-container walt-opacity-min" id="home">
         <div class="walt-display-middle" style="white-space:nowrap;">
-            <span class="walt-center walt-padding-large walt-black walt-xlarge walt-wide walt-animate-opacity">HÉLIO BARROS</span>
+            <span class="walt-center walt-padding-large walt-black walt-xlarge walt-wide walt-animate-opacity">HÉLIO BARROS </span>
         </div>
+        <div id="div_frase" class="walt-display-middle" style="margin-top:100px "><br>
+            <p id="frase"><em>"A ciência é o querer adaptar o menor sonho ao maior."</em><br> <span class="walt-right">Fernando Pessoa</span></p></div>
     </div>
 
     <!-- Container (About Section) -->
-    <div class="walt-content walt-container walt-padding-64" id="about">
+    <div class="walt-content walt-container walt-padding-64" id="sobreHelio">
         <h3 class="walt-center">SOBRE MIM</h3>
         <p> Graduando em engenharia mecânica e sou mecânico de manutenção por formação. Possuo conhecimentos em Autocad, Inventor, Solidworks, Microstation e excel. Obtive meu primeiro contato com a mecânica através de uma oportunidade que alcancei em uma oficina de automoveis ainda muito jovem e a partir deste ponto a curiosidade e a motivaçao para saber mais sobre a area só cresceram e me levaram a cursar o técnico em eletromecânica e logo após a entrar na graduação..</p>
         <div class="walt-row">
             <div class="walt-col m6 walt-center walt-padding-large">
                 <p><b><i class="fa fa-user walt-margin-right"></i>Hélio Barros</b></p><br>
-                <img src="/imagens/perfil.jpg" class="walt-round walt-image walt-opacity walt-hover-opacity-off" alt="Photo of Me" width="500" height="333">
+                <img src="imagens/helio_perfil.png" class="walt-round walt-image walt-opacity walt-hover-opacity-off" alt="Photo of Me" width="500" height="333">
             </div>
 
             <!-- Hide this text on small devices -->
@@ -149,32 +200,55 @@
     <!-- Second Parallax Image with Portfolio Text -->
     <div class="bgimg-2 walt-display-container walt-opacity-min">
         <div class="walt-display-middle">
-            <center><span class="walt-xxlarge walt-text-black walt-wide">FORMAÇÃO ACADÊMICA</span></center>
+            <center><span class="walt-xxlarge walt-text-white walt-wide">FORMAÇÃO ACADÊMICA</span></center>
         </div>
     </div>
 
     <!-- Container (Portfolio Section) -->
-    <div class="walt-content walt-container walt-padding-64" id="portfolio">
+    <div class="walt-content walt-container walt-padding-64" id="formacaoProf">
         <h3 class="walt-center">FORMAÇÃO</h3>
         
         <div class="walt-row-padding walt-center">
            
-         <div class="walt-row-padding walt-center">   
-         <img src="imagens/logoInfoPoli.png" alt="upe-logo"><br>
+         <div class="walt-row-padding walt-center">
+         <div style="margin-bottom: 30px;">
+             <img src="imagens/logoInfoPoli.png" alt="upe-logo"><br>
+        <i style="font-size: 12pt; margin-right: 5px;" class="fa fa-refresh" aria-hidden="true"></i>
            Formação superior (cursando) – Bacharelado em engenharia mecânica industrial, 6º período/Noite (Escola Politécnica de Pernambuco-UPE);
-        </div>
+            </div>
+         </div>   
+         
    
+        <div style="margin-bottom: 30px;">
            
-        <img src="imagens/senai-logo.png" alt="senai-logo"><br>
+            <img src="imagens/senai-logo.png" alt="senai-logo"><br>
+            <i style="font-size: 12pt;  margin-right: 5px;" class="fa fa-graduation-cap" aria-hidden="true"></i>
             Nível profissionalizante – Mecânico de manutenção de máquinas (Concluído em 2012, Escola técnica SENAI - Cabo)  
         </div>
+        </div>   
+        
+        <hr>
        
-        <h3 class="walt-center">EXPERIÊNCIA</h3>
+       <fieldset>
+           <legend style="padding: 5px"><h3 class="walt-center">EXPERIÊNCIA</h3></legend>
         <div class="walt-row-padding walt-center walt-section">
-          <p class="walt-center"><em>Não possuo experiência profissional com carteira assinada ou contrato, estou em busca do aprendizado.</em></p><br>
-            <p class="walt-center">Participei ativamente durante três meses em empresa Junior (Poli Junior Engenharia) em um projeto de adequação a NR12.</p><br>
-            <p class="walt-center">Ministrei aulas durante um ano e seis meses para turmas de pré-vestibular e também acompanhamento escolar.</p> 
+          <p class="walt-center">Estagiário em engenharia mecânica</p>
+          <img src="imagens/prefeitura_recife_logo.png" alt="Prefeitura do Recife">
+          <hr>
+           <div>
+               
+                <p class="walt-center">Participei ativamente como trainee voluntário na Poli Junior Engenharia em um projeto de adequação à NR12.</p>
+                <img style="margin-top: -15px;" width="100" src="imagens/poli-junior.png" alt="poli-junior">   
+           </div>
+           
+           <div>
+               <p class="walt-center">Ministrei aulas durante um ano e seis meses para turmas de pré-vestibular e também acompanhamento escolar.</p>
+            <img width="150" src="imagens/cia_do_estudo.png" alt="cia_do_estudo"> 
+           </div>
+            
         </div>
+       </fieldset>
+        
     </div>
 
     <!-- Modal for full size images on click-->
@@ -194,7 +268,7 @@
     </div>
 
     <!-- Container (Contact Section) -->
-    <div class="walt-content walt-container walt-padding-64" id="contact">
+    <div class="walt-content walt-container walt-padding-64" id="contatoComHelio">
         <h3 class="walt-center">Contato</h3>
         <p class="walt-center"><em>Envie uma mensagem</em></p>
 
@@ -205,23 +279,23 @@
             <div class="walt-col m8 walt-panel">
                 <div class="walt-large walt-margin-bottom">
                     <i class="fa fa-map-marker fa-fw walt-hover-text-black walt-xlarge walt-margin-right"></i> Cabo de Santo Agostinho, PE, Brasil.<br>
-                    <i class="fa fa-phone fa-fw walt-hover-text-black walt-xlarge walt-margin-right"></i> Tel: (81) 9 8441-8408 / (81) 9 88874-0314<br>
+                    <i class="fa fa-phone fa-fw walt-hover-text-black walt-xlarge walt-margin-right"></i> Tel: (81) 9 8441-8408 / (81) 9 8874-0314<br>
                     <i class="fa fa-envelope fa-fw walt-hover-text-black walt-xlarge walt-margin-right"></i> Email: heliomiguel_barros@hotmail.com.br<br>
                 </div>
                 <p>Vamos tomar um café <i class="fa fa-coffee"></i>, entre em contato:</p>
-                <form action="/action_page.php" target="_blank">
+                <form action="enviaMsg.php" method="post">
                     <div class="walt-row-padding" style="margin:0 -16px 8px -16px">
                         <div class="walt-half">
-                            <input class="walt-input walt-border" type="text" placeholder="Name" required name="Nome">
+                            <input class="walt-input walt-border" type="text" placeholder="Name" required name="nome">
                         </div>
                         <div class="walt-half">
-                            <input class="walt-input walt-border" type="text" placeholder="Email" required name="Email">
+                            <input class="walt-input walt-border" type="email" placeholder="Email" required name="email">
                         </div>
                     </div>
-                    <input class="walt-input walt-border" type="text" placeholder="Message" required name="Mensagem">
-                    <button class="walt-button walt-black walt-right walt-section" type="submit">
-          <i class="fa fa-paper-plane"></i> Enviar Mensagem
-        </button>
+                    <input style="margin-bottom: 8px;"class="walt-input walt-border" type="text" placeholder="Assunto" required name="assunto">    
+                    <input class="walt-input walt-border" type="text" placeholder="Mensagem" required name="mensagem">
+                    <input class="walt-button walt-black walt-right walt-section" type="submit" value="Enviar Mensagem">
+         
                 </form>
             </div>
         </div>
@@ -230,11 +304,9 @@
     <!-- Footer -->
     <footer class="walt-center walt-black walt-padding-64 walt-opacity walt-hover-opacity-off">
         <a href="#home" class="walt-button walt-light-grey"><i class="fa fa-arrow-up walt-margin-right"></i>Voltar ao topo</a>
-        <div class="walt-xlarge walt-section">
-            <i class="fa fa-facebook-official walt-hover-opacity"></i>
-            <i class="fa fa-instagram walt-hover-opacity"></i>
-            <i class="fa fa-snapchat walt-hover-opacity"></i>
-            <i class="fa fa-linkedin walt-hover-opacity"></i>
+        <div class="walt-xlarge walt-section" id="redeSocial">
+            <a  href="https://www.facebook.com/helio.miguel.33" target="_blank"><i style="margin-right: 20px;" class="fa fa-facebook-official walt-hover-opacity"></i></a>
+            <a href="https://www.linkedin.com/in/helio-barros-089466a1/" target="_blank"><i class="fa fa-linkedin walt-hover-opacity"></i></a>
         </div>
     </footer>
 
@@ -273,6 +345,13 @@
             }
         }
     </script>
+    <script src="js/minhasRegras.js"></script>
+    
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script src="css/tooltipster/dist/js/tooltipster.bundle.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap-js/bootstrap.min.js"></script>
+    
 
 
 </body>
